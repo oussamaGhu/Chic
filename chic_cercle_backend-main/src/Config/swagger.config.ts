439096772c6e095177e1e0400378bbba052b20a9
oussamaGhu <11779913@GGg>
2025-10-06 16:30:00 +0200

    feat: add Swagger/OpenAPI configuration

---
+++ chic_cercle_backend-main/src/Config/swagger.config.ts
@@ -0,0 +1,15 @@
+// src/swagger.config.ts
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('ChicCircle')
+    .setDescription('tester les Api de chicCircle')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
