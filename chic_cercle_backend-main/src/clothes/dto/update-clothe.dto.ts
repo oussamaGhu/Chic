4ccb08f446f99a79da83443a25cbac76e221df59
oussamaGhu <11779913@GGg>
2025-11-27 10:30:00 +0100

    feat(backend): add Clothes DTOs (create, update)

---
+++ chic_cercle_backend-main/src/clothes/dto/update-clothe.dto.ts
@@ -0,0 +1,35 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateClotheDto } from './create-clothe.dto';
+import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, SchemaTypes, Types } from 'mongoose';
+
+import { Occasion } from "src/clothes/enums/occation.enum";
+import { TypesC } from "src/clothes/enums/TypesC.enum";
+import { Weather } from "src/clothes/enums/weather.enum";
+import { Moods } from '../enums/moods.eum';
+import { User } from 'src/user/schema/usersSchema';
+
+export class UpdateClotheDto extends PartialType(CreateClotheDto) {
+    user: string; // Reference to the user
+    @IsArray()
+    @IsEnum(Occasion, { each: true }) 
+    occasions: Occasion[];
+  
+    images: Types.ObjectId;
+    @IsArray()
+    @IsEnum(Moods, { each: true })
+    moods: Moods[];
+  
+    @IsArray()
+    @IsEnum(Weather, { each: true }) 
+    weather: Weather[];
+  
+    @IsArray()
+    @IsEnum(TypesC, { each: true }) 
+    types: TypesC[];
+  
+    @IsArray()
+    @IsString({ each: true })
+    colors: string[];
+}
